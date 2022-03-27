a9dc51bd958751abe5b19c7d9555112b8e97e753
Sebastián Alvarez <sebagalvarezdev1@hotmail.com>
2022-03-27 10:04:36 -0300

    Ordenado de componentes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import Header from "./Header"
-import Footer from "./Footer"
+import React from 'react';
+import './estilos.css';
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 import Main from "./Main"
-import NavBar from "./NavBar"
-import Contador from "./Contador"
+import NavBar from "./components/NavBar"
+import Contador from "./components/Contador"
 import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
+import {BrowserRouter} from 'react-router-dom';
+
 
 function  App() {
     return (
-<>
-        <Header/>
-        <Footer/>
-        <Main nombre='Sebastian' apellido= 'Alvarez'/>
-        <NavBar/>
-        <ToastContainer />
-       
-</>
+    <BrowserRouter>
+    
+          <NavBar/> 
+          <Header/> 
+          <Footer/> 
+          <Main nombre='Sebastian' apellido= 'Alvarez'/>
+           
+         <ToastContainer />
+    </BrowserRouter>
+
 
     )
 }
